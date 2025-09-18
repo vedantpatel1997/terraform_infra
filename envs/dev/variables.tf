039c6e94c81d6a85ed5c88e1735be3f604bbd5dc
Vedant Patel <84228067+vedantpatel1997@users.noreply.github.com>
2025-09-18 00:49:46 -0600

    Configure service principal authentication

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -6,6 +6,15 @@ variable "tenant_id" {
   type = string
 }
 
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type      = string
+  sensitive = true
+}
+
 variable "hub_subscription_id" {
   type = string
 }
